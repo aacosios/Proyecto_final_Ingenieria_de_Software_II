6c5523c7d1d0d9cda5f0e46860aa13322317accd
aacosios <aacosios@espe.edu.ec>
2022-08-23 22:32:04 -0500

    config

---
+++ tienda/Modelos/mainModel.php
@@ -0,0 +1,16 @@
+<?php
+
+	if($peticion_ajax){
+		require_once "../config/SERVER.php";
+	}else{
+		require_once "./config/SERVER.php";
+	}
+
+	class mainModel{
+
+		/*----------  Funcion conectar a BD - Function connect to BD ----------*/
+		protected static function conectar(){
+			$conexion = new PDO(SGBD,USER,PASS);
+			$conexion->exec("SET CHARACTER SET utf8");
+			return $conexion;
+		} /*--  Fin Funcion - End Function --*/
