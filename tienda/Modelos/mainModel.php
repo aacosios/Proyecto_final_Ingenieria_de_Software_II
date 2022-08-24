096f8c25ea6e52847525d95695c254d76e797634
aacosios <aacosios@espe.edu.ec>
2022-08-23 22:33:05 -0500

    model

--- tienda/Modelos/mainModel.php
+++ tienda/Modelos/mainModel.php
@@ -14,3 +14,19 @@
 			$conexion->exec("SET CHARACTER SET utf8");
 			return $conexion;
 		} /*--  Fin Funcion - End Function --*/
+
+        		/*----------  Funcion desconectar de DB - Function disconnect from DB  ----------*/
+		public function desconectar($consulta){
+			global $conexion, $consulta;
+			$consulta=null;
+			$conexion=null;
+			return $consulta;
+		} /*--  Fin Funcion - End Function --*/
+
+
+		/*----------  Funcion ejecutar consultas simples - Run simple queries function  ----------*/
+		protected static function ejecutar_consulta_simple($consulta){
+			$sql=self::conectar()->prepare($consulta);
+			$sql->execute();
+			return $sql;
+		} /*--  Fin Funcion - End Function --*/
